1d86c904bb08f1feaa575e2c44126906f6458f3a
RalfKonrad <github@eckel-consulting.de>
2023-03-26 23:11:07 +0200

    pom.xml builds but library fails to load

---
+++ wrapper/maven/src/test/java/org/quantlib/LoadingOfLibraryTest.java
@@ -0,0 +1,10 @@
+package org.quantlib;
+
+import org.junit.jupiter.api.Test;
+
+public class LoadingOfLibraryTest {
+    @Test
+    public void testLoading() {
+        Date date = Settings.instance().getEvaluationDate();
+    }
+}
