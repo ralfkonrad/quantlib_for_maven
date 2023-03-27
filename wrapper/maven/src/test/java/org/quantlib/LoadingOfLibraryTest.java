4912b7e368c4f085d7a94ad7d04caae21a9c7cec
RalfKonrad <github@eckel-consulting.de>
2023-03-27 19:15:34 +0200

    Successfully loaded jni windows dll

--- wrapper/maven/src/test/java/org/quantlib/LoadingOfLibraryTest.java
+++ wrapper/maven/src/test/java/org/quantlib/LoadingOfLibraryTest.java
@@ -2,7 +2,16 @@ package org.quantlib;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 public class LoadingOfLibraryTest {
+    static {
+        try {
+            cz.adamh.utils.NativeUtils.loadLibraryFromJar("/org/quantlib/QuantLibJNI.dll");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     @Test
     public void testLoading() {
         Date date = Settings.instance().getEvaluationDate();
