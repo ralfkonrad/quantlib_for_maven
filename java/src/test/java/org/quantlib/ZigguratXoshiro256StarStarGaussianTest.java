1ee0b74084658efb5d91fe9ae992f63ac3a23def
Ralf Konrad <42419984+ralfkonrad@users.noreply.github.com>
2024-06-21 11:39:31 +0200

    Added testGaussianRsgNextValue()

--- java/src/test/java/org/quantlib/ZigguratXoshiro256StarStarGaussianTest.java
+++ java/src/test/java/org/quantlib/ZigguratXoshiro256StarStarGaussianTest.java
@@ -4,11 +4,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ZigguratXoshiro256StarStarGaussianTest {
+    private static final int SEED = 42;
+    private static final double FIRST_EXPECTED = -2.13814;
+    private static final double SECOND_EXPECTED = -0.37490;
+    private static final double TOLERANCE = 1e-5;
+
     @Test
-    public void testNextValue() {
-        var rng = new Xoshiro256StarStarUniformRng(42);
+    public void testGaussianRngNextValue() {
+        var rng = new Xoshiro256StarStarUniformRng(SEED);
         var gaussianRng = new ZigguratXoshiro256StarStarGaussianRng(rng);
-        var expected = -2.13814;
-        Assertions.assertEquals(expected, gaussianRng.next().value(), 1e-5);
+
+        Assertions.assertEquals(FIRST_EXPECTED, gaussianRng.next().value(), TOLERANCE);
+        Assertions.assertEquals(SECOND_EXPECTED, gaussianRng.next().value(), TOLERANCE);
+    }
+
+    @Test
+    public void testGaussianRsgNextValue() {
+        var rng = new Xoshiro256StarStarUniformRng(SEED);
+        var gaussianRng = new ZigguratXoshiro256StarStarGaussianRng(rng);
+        var gaussianRsg = new ZigguratXoshiro256StarStarGaussianRsg(2, gaussianRng);
+        var sample = gaussianRsg.nextSequence();
+
+        Assertions.assertEquals(FIRST_EXPECTED, sample.value().get(0), TOLERANCE);
+        Assertions.assertEquals(SECOND_EXPECTED, sample.value().get(1), TOLERANCE);
     }
 }
